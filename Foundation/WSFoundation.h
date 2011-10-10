cd6b560cc0739546354633b158d533b317689461
Ray Hilton <ray@wirestorm.net>
2011-10-10 11:46:01 +1100

    Add top-level include

---
+++ Foundation/WSFoundation.h
@@ -0,0 +1,13 @@
+//
+//  WSFoundation.h
+//
+//  Created by Ray Hilton on 10/10/11.
+//  Copyright (c) 2011 Wirestorm Pty Ltd. All rights reserved.
+//
+
+
+#import "NSString+Comparison.h"
+#import "NSArray+Collections.h"
+#import "NSArray+TypedAccess.h"
+#import "NSDictionary+TypedAccess.h"
+#import "NSObject+BlockCreate.h"
