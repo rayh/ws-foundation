fa7c7efad36cc72d6437942b3329a7a9795acfde
Ray Hilton <ray@wirestorm.net>
2012-10-19 22:38:49 +1100

    Add UIColor accessors for HSL, RGB and contrast

--- UIKit/UIColor+WSFoundation.h
+++ UIKit/UIColor+WSFoundation.h
@@ -8,6 +8,15 @@
 #import <UIKit/UIKit.h>
 
 @interface UIColor (WSFoundation)
+@property (nonatomic, readonly) CGFloat red;
+@property (nonatomic, readonly) CGFloat green;
+@property (nonatomic, readonly) CGFloat blue;
+@property (nonatomic, readonly) CGFloat alpha;
+@property (nonatomic, readonly) CGFloat hue;
+@property (nonatomic, readonly) CGFloat saturation;
+@property (nonatomic, readonly) CGFloat brightness;
+@property (nonatomic, readonly) CGFloat contrast;
+
 + (UIColor*)colourWithInt:(NSInteger)intValue;
 - (UIColor*)colourByAdjustingHue:(CGFloat)hue saturation:(CGFloat)saturation brightness:(CGFloat)brightness alpha:(CGFloat)alpha;
 
--- UIKit/UIColor+WSFoundation.m
+++ UIKit/UIColor+WSFoundation.m
@@ -16,6 +16,65 @@
                            alpha:1.0];
 }
 
+- (CGFloat)contrast
+{
+    NSArray *rgb = [self rgbValues];
+    CGFloat red = [[rgb objectAtIndex:0] floatValue] * 255;
+    CGFloat green = [[rgb objectAtIndex:1] floatValue] * 255;
+    CGFloat blue = [[rgb objectAtIndex:2] floatValue] * 255;
+    CGFloat yiq = ((red*299)+(green*587)+(blue*114))/1000;
+    return yiq/255.;
+}
+
+- (NSArray*)hsbValues
+{
+    CGFloat originalHue, originalSaturation, originalBrightness, originalAlpha;
+    [self getHue:&originalHue saturation:&originalSaturation brightness:&originalBrightness alpha:&originalAlpha];
+    return @[@(originalHue), @(originalSaturation), @(originalBrightness), @(originalAlpha)];
+}
+
+- (NSArray*)rgbValues
+{
+    CGFloat red,green,blue, alpha;
+    [self getRed:&red green:&green blue:&blue alpha:&alpha];
+    return @[@(red), @(green), @(blue), @(alpha)];
+}
+
+- (CGFloat)hue
+{
+    return [[[self hsbValues] objectAtIndex:0] floatValue];
+}
+
+- (CGFloat)saturation
+{
+    return [[[self hsbValues] objectAtIndex:1] floatValue];
+}
+
+- (CGFloat)brightness
+{
+    return [[[self hsbValues] objectAtIndex:2] floatValue];
+}
+
+- (CGFloat)alpha
+{
+    return [[[self hsbValues] objectAtIndex:3] floatValue];
+}
+
+- (CGFloat)red
+{
+    return [[[self rgbValues] objectAtIndex:0] floatValue];
+}
+
+- (CGFloat)green
+{
+    return [[[self rgbValues] objectAtIndex:1] floatValue];
+}
+
+- (CGFloat)blue
+{
+    return [[[self rgbValues] objectAtIndex:2] floatValue];
+}
+
 - (UIColor*)colourByAdjustingHue:(CGFloat)hue saturation:(CGFloat)saturation brightness:(CGFloat)brightness alpha:(CGFloat)alpha
 {
     CGFloat originalHue, originalSaturation, originalBrightness, originalAlpha;
